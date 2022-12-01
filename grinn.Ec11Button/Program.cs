83d2bb84ee75ac127140b2b96d7c0921a0bafd3d
Andries <Andries.Schaap@rakuten.com>
2022-12-01 17:30:09 +0000

    Initial commit

---
+++ grinn.Ec11Button/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Device.Gpio;
+
+Console.WriteLine("Hello, World!");
+
+const int encoderPinA = 12;
+const int encoderPinB = 11;
+
+using var controller = new GpioController();
+controller.OpenPin(encoderPinA, PinMode.Input, PinValue.High);
+controller.OpenPin(encoderPinB, PinMode.Input, PinValue.High);
+
+controller.RegisterCallbackForPinValueChangedEvent(encoderPinA, 
+    PinEventTypes.Falling | PinEventTypes.Rising, OnPinEvent);
+controller.RegisterCallbackForPinValueChangedEvent(encoderPinB, 
+    PinEventTypes.Falling | PinEventTypes.Rising, OnPinEvent);
+
+static void OnPinEvent(object sender, PinValueChangedEventArgs args)
+{
+    Console.WriteLine($"{DateTime.Now} - Pin: {args.PinNumber}, EventType: {args.ChangeType}");
+}
