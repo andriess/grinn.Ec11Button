3f08c274edf8eff12e17754583c940bfe8d0694b
Andries <Andries.Schaap@rakuten.com>
2022-12-02 14:49:47 +0000

    Added some mpd stuff.

--- grinn.Ec11Button/Program.cs
+++ grinn.Ec11Button/Program.cs
@@ -1,7 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using grinn.Ec11Button;
-using Iot.Device.RotaryEncoder;
 
 Console.WriteLine("Hello, World!");
 
@@ -9,10 +11,25 @@ const int encoderPinA = 17;
 const int encoderPinB = 18;
 const int encoderPinC = 27;
 
+// My single button code. This should move somewhere smarter once we start orchestrating our gui+controls
 var rotaryButton = new CustomRotaryEncoder(encoderPinA, encoderPinB, encoderPinC, 20);
 rotaryButton.Debounce = TimeSpan.FromMilliseconds(175);
 rotaryButton.OnEncoderChange += HandleEncoderChange;
 rotaryButton.OnClick += HandleClick;
+
+// Trying some MPD stuff here: 
+var mpdEndpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 6600);
+var tcpClient = new TcpClient();
+await tcpClient.ConnectAsync(mpdEndpoint);
+
+var connectionStream = tcpClient.GetStream();
+var reader = new StreamReader(connectionStream, Encoding.ASCII);
+var writer = new StreamWriter(connectionStream, Encoding.ASCII) { NewLine = "\n"};
+
+var isConnected = reader.ReadLine();
+var version = isConnected.Substring(7);
+
+Console.Write(version);
 
 await Task.Delay(Timeout.Infinite);
 
