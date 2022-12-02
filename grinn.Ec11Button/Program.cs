23093845a73431fa12c955646d59cdfcc194e418
Andries <Andries.Schaap@rakuten.com>
2022-12-02 10:54:28 +0000

    even more debounce

--- grinn.Ec11Button/Program.cs
+++ grinn.Ec11Button/Program.cs
@@ -9,7 +9,7 @@ const int encoderPinA = 17;
 const int encoderPinB = 18;
 
 var rotaryButton = new CustomRotaryEncoder(encoderPinA, encoderPinB, 20);
-rotaryButton.Debounce = TimeSpan.FromMilliseconds(50);
+rotaryButton.Debounce = TimeSpan.FromMilliseconds(250);
 rotaryButton.PulseCountChanged += HandlePulseCountChange;
 rotaryButton.OnEncoderChange += HandleEncoderChange;
 
