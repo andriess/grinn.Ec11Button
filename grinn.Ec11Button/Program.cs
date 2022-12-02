7d23bb9d7797496d15169b8530f6bc960d839113
Andries <Andries.Schaap@rakuten.com>
2022-12-02 09:36:23 +0000

    Why invent the wheel yourself, if you can use a library.

--- grinn.Ec11Button/Program.cs
+++ grinn.Ec11Button/Program.cs
@@ -1,29 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Device.Gpio;
+using Iot.Device.RotaryEncoder;
 
 Console.WriteLine("Hello, World!");
 
 const int encoderPinA = 17;
 const int encoderPinB = 18;
 
-using var controller = new GpioController();
-controller.OpenPin(encoderPinA, PinMode.Input, PinValue.High);
-controller.OpenPin(encoderPinB, PinMode.Input, PinValue.High);
-
-controller.RegisterCallbackForPinValueChangedEvent(encoderPinA, 
-    PinEventTypes.Falling | PinEventTypes.Rising, OnPinEvent);
-/*controller.RegisterCallbackForPinValueChangedEvent(encoderPinB, 
-    PinEventTypes.Falling | PinEventTypes.Rising, OnPinEvent);*/
+var rotaryButton = new QuadratureRotaryEncoder(encoderPinA, encoderPinB, PinEventTypes.Falling, 20);
+rotaryButton.Debounce = TimeSpan.FromMilliseconds(10);
+rotaryButton.PulseCountChanged += OnPulseCountChanged;
 
 await Task.Delay(Timeout.Infinite);
 
-void OnPinEvent(object sender, PinValueChangedEventArgs args)
+void OnPulseCountChanged(object? sender, RotaryEncoderEventArgs args)
 {
-    var otherPin = args.PinNumber == encoderPinA ? encoderPinA : encoderPinB;
-    var otherPinValue = controller.Read(otherPin);
-    var currentPinValue = controller.Read(args.PinNumber);
+    var counter = Math.Abs(args.Value - (-1)) < 0.1 ? "Counter" : string.Empty;
+    Console.WriteLine($"{DateTime.Now} - {counter} clockwise ");
 
-    Console.WriteLine($"{DateTime.Now} - Pin: {args.PinNumber}, EventType: {args.ChangeType}, CurrentPinValue: {currentPinValue}, OtherPinValue: {otherPinValue}");
-    Task.Delay(10);
 }
