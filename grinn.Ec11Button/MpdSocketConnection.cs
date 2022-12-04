5f34d6279a7283ea73704a6e726c0168a8a8ef5b
Andries <Andries.Schaap@rakuten.com>
2022-12-04 14:08:01 +0000

    Moved the mpd code into a class.

---
+++ grinn.Ec11Button/MpdSocketConnection.cs
@@ -0,0 +1,64 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace grinn.Ec11Button;
+
+public class MpdSocketConnection
+{
+    private static readonly byte[] ReceivedBytes = new byte[256];
+    private static readonly char[] ReceivedChars = new char[256];
+
+    private static string _socketPath;
+    private readonly Socket _socket;
+    private readonly UnixDomainSocketEndPoint _endpoint;
+
+    public MpdSocketConnection(string socketPath)
+    {
+        _socketPath = socketPath;
+        _socket = new Socket(AddressFamily.Unix, SocketType.Stream, ProtocolType.IP);
+        _endpoint = new UnixDomainSocketEndPoint(_socketPath);
+    }
+
+    public async Task Connect()
+    {
+        await _socket.ConnectAsync(_endpoint);
+        var connectionResponse = await ReceiveResponseFromSocket();
+
+        if (!connectionResponse.StartsWith("OK MPD "))
+        {
+            throw new Exception($"Could not connect to MPD. Response: {connectionResponse}");
+        }
+    }
+    
+    public async Task<string> SendCommandToSocket(string command)
+    {
+        if (!_socket.Connected)
+            await Connect();
+        
+        var requestBytes = Encoding.ASCII.GetBytes(command);
+        var bytesSend = 0;
+        while (bytesSend < requestBytes.Length)
+        {
+            bytesSend += await _socket.SendAsync(requestBytes.AsMemory(bytesSend), SocketFlags.None);
+        }
+
+        return await ReceiveResponseFromSocket();
+    }
+    
+    private async Task<string> ReceiveResponseFromSocket()
+    {
+        Array.Clear(ReceivedBytes);
+        Array.Clear(ReceivedChars);
+
+        var bytesReceived = await _socket.ReceiveAsync(ReceivedBytes, SocketFlags.None);
+    
+        Console.WriteLine($"{nameof(ReceiveResponseFromSocket)} - Bytes received: {bytesReceived}");
+        // Convert byteCount bytes to ASCII characters using the 'responseChars' buffer as destination
+        Encoding.ASCII.GetChars(ReceivedBytes, 0, bytesReceived, ReceivedChars, 0);
+
+        var responseString = new string(ReceivedChars);
+        Console.WriteLine($"{nameof(ReceiveResponseFromSocket)} - Response string: {responseString}");
+
+        return responseString;
+    }
+}
--- grinn.Ec11Button/Program.cs
+++ grinn.Ec11Button/Program.cs
@@ -1,8 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
 using grinn.Ec11Button;
 
 Console.WriteLine("Hello, World!");
@@ -18,62 +15,13 @@ rotaryButton.OnClick += HandleClick;
 
 
 // Trying some MPD stuff here: 
-const string unixSocketPath = "/var/run/mpd/socket";
-using var socket = new Socket(AddressFamily.Unix, SocketType.Stream, ProtocolType.IP);
-var endpoint = new UnixDomainSocketEndPoint(unixSocketPath);
-await socket.ConnectAsync(endpoint);
+var mpdConnection = new MpdSocketConnection("/var/run/mpd/socket");
+var result = await mpdConnection.SendCommandToSocket("ping\n");
 
-var receivedBytes = new byte[256];
-var receivedChars = new char[256];
+Console.WriteLine(result);
 
-var onConnectedResponse = await ReceiveResponseFromSocket(socket, receivedBytes, receivedChars);
-Console.WriteLine(onConnectedResponse);
-
-SendCommandToSocket(socket, "ping\n");
-
-var onPingResponse = await ReceiveResponseFromSocket(socket, receivedBytes, receivedChars);
-Console.WriteLine($"Program - OnPingResponse: {onPingResponse}");
-
-static async Task<string> ReceiveResponseFromSocket(Socket socket, byte[] receivedBytes, char[] receivedChars)
-{
-    #region Validate Arguments
-        if (socket == null)
-            throw new ArgumentNullException(nameof(socket), "Socket cannot be null.");
-    #endregion
-    
-    Array.Clear(receivedBytes);
-    Array.Clear(receivedChars);
-    
-    Console.WriteLine($"{nameof(ReceiveResponseFromSocket)} - ReceivedBytes Size: {receivedBytes.Length}, ReceivedChars Size: {receivedChars.Length}");
-    
-    var bytesReceived = await socket.ReceiveAsync(receivedBytes, SocketFlags.None);
-    
-    Console.WriteLine($"{nameof(ReceiveResponseFromSocket)} - Bytes received: {bytesReceived}");
-    // Convert byteCount bytes to ASCII characters using the 'responseChars' buffer as destination
-    Encoding.ASCII.GetChars(receivedBytes, 0, bytesReceived, receivedChars, 0);
-
-    var responseString = new string(receivedChars);
-    Console.WriteLine($"{nameof(ReceiveResponseFromSocket)} - Response string: {responseString}");
-
-    return responseString;
-}
-
-static void SendCommandToSocket(Socket socket, string command)
-{
-    #region Validate Arguments
-        if (string.IsNullOrEmpty(command))
-            throw new ArgumentNullException(nameof(command), "Command cannot be null or empty.");
-        if (socket == null)
-            throw new ArgumentNullException(nameof(socket), "Socket cannot be null.");
-    #endregion
-    
-    var requestBytes = Encoding.ASCII.GetBytes(command);
-    var bytesSend = 0;
-    while (bytesSend < requestBytes.Length)
-    {
-        bytesSend += socket.Send(requestBytes, bytesSend, requestBytes.Length - bytesSend, SocketFlags.None);
-    }
-}
+var playlistInfo = await mpdConnection.SendCommandToSocket("playlistinfo\n");
+Console.WriteLine(playlistInfo);
 
 void HandleClick(object? sender, bool args)
 {
