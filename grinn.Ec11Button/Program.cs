3de458992f5757dca62ff035f4f084972c6cf085
Andries <Andries.Schaap@rakuten.com>
2022-12-04 12:35:56 +0000

    Not so pretty with the non async handler there. But I needed a way to trigger the ping to see the socket response.

--- grinn.Ec11Button/Program.cs
+++ grinn.Ec11Button/Program.cs
@@ -11,12 +11,6 @@ const int encoderPinA = 17;
 const int encoderPinB = 18;
 const int encoderPinC = 27;
 
-// My single button code. This should move somewhere smarter once we start orchestrating our gui+controls
-var rotaryButton = new CustomRotaryEncoder(encoderPinA, encoderPinB, encoderPinC, 20);
-rotaryButton.Debounce = TimeSpan.FromMilliseconds(175);
-rotaryButton.OnEncoderChange += HandleEncoderChange;
-rotaryButton.OnClick += HandleClick;
-
 // Trying some MPD stuff here: 
 const string unixSocketPath = "/var/run/mpd/socket";
 
@@ -26,16 +20,24 @@ await socket.ConnectAsync(endpoint);
 
 var receivedBytes = new byte[256];
 var receivedChars = new char[256];
-var bytesReceived = await socket.ReceiveAsync(receivedBytes, SocketFlags.None);
-
-if (bytesReceived != 0)
+while (true)
 {
+    var bytesReceived = await socket.ReceiveAsync(receivedBytes, SocketFlags.None);
+
+    if (bytesReceived == 0) break;
+    
     // Convert byteCount bytes to ASCII characters using the 'responseChars' buffer as destination
     int charCount = Encoding.ASCII.GetChars(receivedBytes, 0, bytesReceived, receivedChars, 0);
 
     // Print the contents of the 'responseChars' buffer to Console.Out
     await Console.Out.WriteAsync(receivedChars.AsMemory(0, charCount));
 }
+
+// My single button code. This should move somewhere smarter once we start orchestrating our gui+controls
+var rotaryButton = new CustomRotaryEncoder(encoderPinA, encoderPinB, encoderPinC, 20);
+rotaryButton.Debounce = TimeSpan.FromMilliseconds(175);
+rotaryButton.OnEncoderChange += HandleEncoderChange;
+rotaryButton.OnClick += HandleClick;
 
 /*var mpdEndpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 6600);
 var tcpClient = new TcpClient();
@@ -52,6 +54,13 @@ await Task.Delay(Timeout.Infinite);
 
 void HandleClick(object? sender, bool args)
 {
+    var requestBytes = Encoding.ASCII.GetBytes("ping");
+    var bytesSend = 0;
+    while (bytesSend < requestBytes.Length)
+    {
+        bytesSend += socket.Send(requestBytes, bytesSend, requestBytes.Length - bytesSend, SocketFlags.None);
+    }
+        
     Console.WriteLine($"{nameof(HandleClick)} - click event arrived in main program.");
 }
 
